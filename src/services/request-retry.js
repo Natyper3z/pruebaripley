3b0552cc7fb7a046fe87b693a71b8ec82de54cfe
Natalia <npnataliaperez@gmail.com>
2019-10-05 21:22:07 -0300

    Commit

--- src/services/request-retry.js
+++ src/services/request-retry.js
@@ -1,14 +1,14 @@
 import request from 'request';
 
-const SIMULATED_ERROR_RATE = 10; // Represents 10%
+const SIMULATED_ERROR_RATE = 10; // 10% de error
 
 const requestRetrier = options => new Promise((resolve) => {
   request(options, (error, r, response) => {
     if (error) {
       return resolve(requestRetrier(options));
     }
-    // Generate an integer between 0 and 100
-    // If it is below SIMULATED_ERROR_RATE, we throw an error
+    // Genera un INT  entre 0 y 100
+    // Error
     if (Math.floor(Math.random() * 100) < SIMULATED_ERROR_RATE) {
       return resolve(requestRetrier(options));
     }
